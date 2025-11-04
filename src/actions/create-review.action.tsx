64444d98dc98ceef656d429a8c37b3c7e4203dd0
pine1225 <112848935+coke709@users.noreply.github.com>
2025-11-04 15:17:46 +0900

    57강까지

--- src/actions/create-review.action.tsx
+++ src/actions/create-review.action.tsx
@@ -1,15 +1,22 @@
 "use server";
 
-export async function createReviewAction(formData: FormData) {
+import { delay } from "@/util/delay";
+import { revalidatePath, revalidateTag } from "next/cache";
+
+export async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요",
+    };
   }
 
   try {
+    await delay(2000);
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
       {
@@ -17,9 +24,37 @@ export async function createReviewAction(formData: FormData) {
         body: JSON.stringify({ bookId, content, author }),
       }
     );
-    console.log(response.status);
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // Next 서버측에게 다시 재검증해줄 것을 요청하는 메서드
+    // 실시간으로 다이나믹 페이지로 생성하게 된다 (풀라우트도 삭제)
+
+    // 1. 특정 주소의 해당하는 페이지만 재검증
+    // revalidatePath(`/book/${bookId}`);
+
+    // 2. 특정 경로의 모든 동적 페이지를 재검증
+    // 모든 도서의 페이지를 재검증...
+    // revalidatePath(`/book/[id]`, "page");
+
+    // 3. 특정 레이아웃을 갖는 모든 페이지 재검증
+    // revalidatePath("/(with-searchbar", "layout");
+
+    // 4. 모든 데이터 재검증
+    // revalidatePath("/", "layout");
+
+    // 5. 태그 기준, 데이터 캐시 재검증
+    revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${err}`,
+    };
   }
 }
--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -41,7 +41,7 @@ async function AllBooks() {
 async function RecoBooks() {
   await delay(3000);
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/random`,
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } }
   );
   if (!response.ok) {
